86670ef946440efd40e09060507a4b22d648d6ed
Ammar Ahmed <143522718+ammar223ahmed@users.noreply.github.com>
2024-02-22 15:50:17 +0500

    Add files via upload

---
+++ Jawa Script/app.js
@@ -0,0 +1,119 @@
+// CHAPTER NO: 1
+
+// Q NO : 1
+
+alert('WELCOME')
+// Q NO : 2
+alert('Error! please enter a valid password')
+
+// Q NO : 3
+    alert('Welcome to Js land Happy Coding')
+
+// Q NO : 4 
+
+alert('Welcome to js land')
+alert('Happy Codding Prevent this page foam creating additional dilogs:')
+
+// Q NO : 5 
+
+alert('Hello... I can run JS through my web browsers console')
+
+
+// Q NO : 6
+
+alert("HTML / CSS Project")
+
+
+// Q NO : 7
+
+// b: Body (After your page's HTML)
+
+
+// CHAPTER NO: 1 THE END
+
+// CHAPTER NO: 2
+
+// Q NO : 1
+
+var userName 
+
+// Q NO : 2
+
+var myName = "Ammar Ahmed"
+
+// Q NO : 3
+
+var JS = "Hello World"
+
+alert(JS)
+
+// Q NO : 4
+
+var usersName = "Ammar ahmed"
+var myAge = "15 years old"
+var myCertificate = "Mobile Application Development"
+
+alert(usersName)
+alert(myAge)
+alert(myCertificate)
+
+// Q NO : 5
+alert("PIZZA , PIZZ , PLZ , PL , P")
+
+
+// Q NO : 6
+
+var email = "My email address is dirleader123@gmail.com"
+alert(email)
+
+// Q NO : 7
+
+var book = "I am trying to learn from the Book A Smarter way to learn JavaScript"
+alert(book)
+
+// Q NO : 8
+
+/* <h2>Yah! I can write HTML content through JavaScript</h2> */
+
+// Q NO : 9
+
+
+alert("--------*.^.^.*--------")
+
+
+// CHAPTER NO: 2 THE END
+
+
+// CHAPTER NO: 3
+ 
+
+// Q NO : 1
+var age = "I am 15 years old"
+alert(age)
+
+// Q NO : 2
+var track = "You have visited this site 14 times"
+alert(track)
+ 
+// Q NO : 3
+
+var birthYear = 2008
+alert(birthYear)
+
+// Q NO : 4
+
+var clothingStore = "MRH STORE"
+var visitorName = "ZARA BRAND"
+var productTitle = "T-shirts"
+var ordered =  "Ammar Ahmed ordered by ZARA Brand 5 T-shirts(medium) size ....."
+
+alert(ordered)
+
+
+
+// CHAPTER NO: 3 THE END
+
+// CHAPTER NO: 4
+
+// Q NO : 1
+
